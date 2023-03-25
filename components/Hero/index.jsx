ec8d497b1120d0258ebf988f15958e88949e8e70
Donald <Djp0242@gmail.com>
2023-03-25 11:35:36 -0700

    landing page but with questions

---
+++ components/Hero/index.jsx
@@ -0,0 +1,40 @@
+const HeroContainer = ({
+	ClassDiv,
+	addMoving,
+	title,
+	textUp,
+	SpanTex,
+	textDown,
+	linkUp,
+	linkDown
+}) => {
+	return(
+	    <section className={ClassDiv} id="home">
+	        {addMoving && <div className="moving-bg"></div>}
+	        <div className="hero-section-content">
+	            <div className="container ">
+	                <div className="row align-items-center">
+	                    <div className="col-12 col-lg-6 col-md-12">
+	                        <div className="welcome-content">
+	                            <div className="promo-section">
+	                                <h3 className="special-head gradient-text">{title}</h3>
+	                            </div>
+                                <h1>Chain of Impact</h1>
+                                <br></br>
+	                            <h1>{textUp} <span className="gradient-t">{SpanTex}</span> </h1>
+	                            <p className="w-text">{textDown}</p>
+	                            <div className="dream-btn-group">
+                                  <a href='https://github.com/jackson-harris-iii/scaling-ethereum-2023' target="_blank" className="btn btn-explore more-btn mr-3">Github</a>
+                                  <a href='https://ethglobal.com/showcase/chain-of-impact-knbts' target="_blank" className="btn btn-explore more-btn mr-3">Project</a>
+                              </div>
+	                        </div>
+	                    </div>
+	                    <div className="col-lg-6"></div>
+	                </div>
+	            </div>
+	        </div>
+	    </section>
+	)
+}
+
+export default HeroContainer
